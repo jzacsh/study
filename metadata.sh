5859a5cd5f510c9fa651c97f418d1299100539e8
Jonathan Zacsh <jzacsh@gmail.com>
2016-10-07 18:55:10 -0400

    only metadata planned - so far - for overall tree

---
+++ metadata.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+setsRoot="$(readlink -f "$1")"; declare -r setsRoot
+
+set -x
+find "$setsRoot" -mindepth 1 -maxdepth 1 -type d -printf '%P\n' | tee -a "$setsRoot".index
