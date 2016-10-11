jzacsh/study

commit f025b11be88ed0b87ddcf48180919dd9755002b3

<tr> injection: is-updated check base on entire JSON string

static/index.js
@@ -322,16 +322,16 @@ let storageGetBlob = function(key) {
   return Boolean(val && val.length) ? JSON.parse(val) : null;
 };
 
-let lastUrlsLen = -1;
+let lastUrlsEntry = '';
 let refreshDashboardUi = function() {
   let urls = storageGetBlob('URLS');
   if (!urls) {
     return;
   }
-  if (Object.keys(urls).length == lastUrlsLen) {
+  if (urls == lastUrlsEntry) {
     return;
   }
-  lastUrlsLen = Object.keys(urls).length;
+  lastUrlsEntry = urls;
 
   if (urls['cards.index'] && urls['cards.index'].length) {
     document.querySelectorAll('[data-qty-sets]').forEach(el => {

commit 9410be3d50451f054651e0d1d7c10cfcc6eb32ad

major bug fix: stop infinite loop caused by f025b11be88ed

static/index.js
@@ -329,14 +329,15 @@ let storageGetBlob = function(key) {
 
 let lastUrlsEntry = '';
 let refreshDashboardUi = function() {
-  let urls = storageGetBlob('URLS');
-  if (!urls) {
+  let urls = localStorage.getItem('URLS');
+  if (!(urls && urls.length)) {
     return;
   }
   if (urls == lastUrlsEntry) {
     return;
   }
   lastUrlsEntry = urls;
+  urls = JSON.parse(urls);
 
   if (urls['cards.index'] && urls['cards.index'].length) {
     document.querySelectorAll('[data-qty-sets]').forEach(el => {