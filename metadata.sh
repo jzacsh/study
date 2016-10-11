6a567a1d5260b0169ec3c4c450ee50f4f68dd897
Jonathan Zacsh <jzacsh@gmail.com>
2016-10-10 23:44:42 -0400

    bug fix: *overwrite* cardsets' index when refreshing it

--- metadata.sh
+++ metadata.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 setsRoot="$(readlink -f "$1")"; declare -r setsRoot
 
 set -x
-find "$setsRoot" -mindepth 1 -maxdepth 1 -type d -printf '%P\n' | tee -a "$setsRoot".index
+find "$setsRoot" -mindepth 1 -maxdepth 1 -type d -printf '%P\n' | tee "$setsRoot".index
